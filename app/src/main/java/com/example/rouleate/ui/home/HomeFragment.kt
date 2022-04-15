cfea72c76558bcc7a717b85b0bcd19003a0bfc0b
Julio Alva <[julioalva1992@gmail.com]>
2022-04-15 18:30:25 -0400

    Set up the enter choices layout, created Activity to handle the logic of it

--- app/src/main/java/com/example/rouleate/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/rouleate/ui/home/HomeFragment.kt
@@ -7,9 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.rouleate.R
 import com.example.rouleate.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment()  {
 
     private var _binding: FragmentHomeBinding? = null
 
@@ -33,6 +34,17 @@ class HomeFragment : Fragment() {
             textView.text = it
         }
         return root
+    }
+
+    /*
+    * 4/14/2022:
+    * This function will load the add_choices_activity layout
+    * Need a button widget in the fragment_home layout that activates the transition
+    * Maybe this is not the correct place to have this transition because
+    * setContentView is needed, and this function is not part of the fragment class
+    * */
+    fun loadAddChoicesLayout(v: View){
+        // TODO: define function, add a button widget
     }
 
     override fun onDestroyView() {
