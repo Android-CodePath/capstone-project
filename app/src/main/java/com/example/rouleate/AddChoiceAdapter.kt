17df94c84f3005a05722aa5910e49cd9fe08737e
Julio Alva <[julioalva1992@gmail.com]>
2022-04-22 17:43:58 -0400

    Added logic to handle input choices

---
+++ app/src/main/java/com/example/rouleate/AddChoiceAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.rouleate
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/*
+* Tells the recyclerView how to display the data that is given
+* This is needed to bridge the reclycerview
+* */
+class AddChoiceAdapter(val listOfChoices: List<String>): RecyclerView.Adapter<AddChoiceAdapter.ViewHolder>() {
+
+    // Inflating(create) the layout from the xml file and return the holder 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+
+        return ViewHolder(contactView)
+    }
+
+    // Populate layout inside viewHolder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+
+        val item = listOfChoices.get(position)
+
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listOfChoices.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        // Store references to elements in the layout view
+        val textView: TextView
+
+        init{
+            textView = itemView.findViewById(android.R.id.text1)
+        }
+
+    }
+}
